e27bdece950ee8bc3eb0a8adf14e5a1c70f6ad55
Royden Shin Tan <roydensynderen@gmail.com>
2018-09-16 16:42:09 -0700

    Practice 1

--- Practice/format.py
+++ Practice/format.py
@@ -11,3 +11,11 @@ print('The {q} {b} {f}'.format(f='fox', b='brown', q='quick'))
 #Result: The quick brown fox
 
 #FLOAT FORMATING FOLLOWS '{value: width.precision f}'
+result = 104.12345
+print(result)
+print('The result was {r:1.2f}'.format(r=result))
+
+#Another way to do it; released in Python 3.6
+name = "Jose"
+print(f'Hello, his name is {name}')
+
